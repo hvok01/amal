66bf394e18097ee73a245461c0befbcc24102c5e
hvok01 <leonelgomezrodriguez@hotmail.com>
2023-09-25 12:14:31 -0300

    navbar work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css'
 
 function App() {
+  //https://amal-fluid-demo.squarespace.com/
 
   return (
     <>
-      amal
+      <header>
+        <nav>
+          <div className="site-logo">
+            <a href="#">Amal</a>
+          </div>
+          <div className="site-hamburger">
+            <span className="line-one"></span>
+            <span className="line-two"></span>
+          </div>
+          <div className="site-navlinks">
+            <ul>
+              <li><a href="#">Speaking events</a></li>
+              <li><a href="#" className="abs-link">Contact Me</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </>
   )
 }
